7e43d67bc51fddfe504caddf3eb3c0ecc7382132
OgneFF <wyrik9324@gmail.com>
2025-10-28 14:52:58 +0300

    исправил типы у фото

--- src/Data/SliderRegistratiom.ts
+++ src/Data/SliderRegistratiom.ts
@@ -1,5 +1,5 @@
 export const mapImages = [
-  { id: 1, image: "/img1.jpeg", title: "Первая" },
+  { id: 1, image: "/img1.jpg", title: "Первая" },
   { id: 2, image: "/img2.jpg", title: "Вторая" },
-  { id: 3, image: "/img3.jpeg", title: "Третья" },
+  { id: 3, image: "/img3.jpg", title: "Третья" },
 ];
